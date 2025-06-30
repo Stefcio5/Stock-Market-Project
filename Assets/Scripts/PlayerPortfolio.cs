2e291c2a2253d20749453750b2db09657c2360eb
Stefcio5 <tefcio5@gmail.com>
2025-07-01 01:54:18 +0200

    Refactored PlayerPortfolio to encapsulate player cash, added methods for total stock value, total value, and profit percentage calculations

--- Assets/Scripts/PlayerPortfolio.cs
+++ Assets/Scripts/PlayerPortfolio.cs
@@ -1,29 +1,37 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerPortfolio : MonoBehaviour
 {
-    public float playerCash = 10000f;
+    [SerializeField] private float _initialCash = 10000f;
+    private float _playerCash;
+    public float PlayerCash
+    {
+        get => _playerCash;
+        private set => _playerCash = value;
+    }
     private MarketManager marketManager;
 
     public void Init(MarketManager marketManager)
     {
         this.marketManager = marketManager;
+        _playerCash = _initialCash;
     }
 
     public void BuyStock(StockDataSO stockData, int quantity)
     {
         var stock = marketManager.GetStock(stockData);
         float totalCost = stock.CurrentPrice * quantity;
-        if (totalCost > playerCash)
+        if (totalCost > PlayerCash)
         {
             Debug.LogWarning("Not enough cash to buy " + quantity + " shares of " + stockData.stockName);
             return;
         }
-        stock.BuyShares(quantity, ref playerCash);
+        stock.BuyShares(quantity, ref _playerCash);
 
-        GameEvents.RaiseOnCashChanged(playerCash);
+        GameEvents.RaiseOnCashChanged(PlayerCash);
         GameEvents.RaiseOnStockBought(stock, quantity);
-        Debug.Log("Bought " + quantity + "shares of " + stockData.stockName + ". Remaining cash: " + playerCash);
+        Debug.Log("Bought " + quantity + "shares of " + stockData.stockName + ". Remaining cash: " + PlayerCash);
     }
 
     public void SellStock(StockDataSO stockData, int quantity)
@@ -34,9 +42,9 @@ public class PlayerPortfolio : MonoBehaviour
             Debug.LogWarning("Not enough shares to sell " + quantity + " shares of " + stockData.stockName);
             return;
         }
-        stock.SellShares(quantity, ref playerCash);
+        stock.SellShares(quantity, ref _playerCash);
 
-        GameEvents.RaiseOnCashChanged(playerCash);
+        GameEvents.RaiseOnCashChanged(PlayerCash);
         GameEvents.RaiseOnStockSold(stock, quantity);
     }
 
@@ -44,12 +52,33 @@ public class PlayerPortfolio : MonoBehaviour
     {
         var stock = marketManager.GetStock(stockData);
         float totalCost = stock.CurrentPrice * quantity;
-        return totalCost <= playerCash;
+        return totalCost <= PlayerCash;
     }
 
     public bool CanSellStock(StockDataSO stockData, int quantity)
     {
         var stock = marketManager.GetStock(stockData);
         return stock.OwnedShares >= quantity;
+    }
+
+    public float GetTotalStockValue()
+    {
+        float totalStockValue = 0f;
+        foreach (var stock in marketManager.Stocks.Values)
+        {
+            totalStockValue += stock.CurrentPrice * stock.OwnedShares;
+        }
+        return totalStockValue;
+    }
+
+    public float GetTotalValue()
+    {
+        return PlayerCash + GetTotalStockValue();
+    }
+
+    public float GetProfitPercentage()
+    {
+        float totalValue = GetTotalValue();
+        return (totalValue - _initialCash) / _initialCash * 100f;
     }
 }
